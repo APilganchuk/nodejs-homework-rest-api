af6e18779d02088215bd03012e8dc3a61ba70378
APilganchuk <opilganchuk@gmail.com>
2021-10-19 13:15:39 +0300

    feat: done hw4

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,7 +5,10 @@ const { auth: ctrl } = require("../../controllers");
 const router = express.Router();
 
 router.post("/register", ctrl.register);
+// /signup
 router.post("/login", ctrl.login);
+// /signin
 router.get("/logout", authenticate, ctrl.logout);
+// /signout
 
 module.exports = router;
