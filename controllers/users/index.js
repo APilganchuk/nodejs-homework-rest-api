be88de1f7c0f404264b44da4aca7bd0fe139cdfe
APilganchuk <opilganchuk@gmail.com>
2021-10-25 13:23:52 +0300

    feat : done hw5

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,4 +1,5 @@
 const getCurrent = require("./current");
 const updateSubscription = require("./updateSubscription");
+const updateAvatar = require("./updateAvatar");
 
-module.exports = { getCurrent, updateSubscription };
+module.exports = { getCurrent, updateSubscription, updateAvatar };
